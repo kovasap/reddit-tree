22fa49e06be2703573c92f722c1a65080cb6fbe9
Kovas Palunas <kovas.palunas@gmail.com>
2021-08-21 23:18:23 -0700

    print comments tree

--- src/reddit_tree/core.cljs
+++ src/reddit_tree/core.cljs
@@ -1,7 +1,32 @@
 (ns reddit-tree.core
-    (:require
-      [reagent.core :as r]
-      [reagent.dom :as d]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require
+   [cljs-http.client :as http]
+   [cljs.core.async :refer [<!]]
+   [reagent.core :as r]
+   [reagent.dom :as d]))
+
+
+;; Useful for determining structure: https://jsonformatter.curiousconcept.com/#
+(defn print-comment-bodies [comments-json]
+
+  ;; (prn "call" (keys comments-json))
+  (prn (:body comments-json))
+  ;; json can either have :data or :comments, in alternating sub-dicts
+  (if (:data comments-json)
+     (print-comment-bodies (:data comments-json))
+     (if (:children comments-json)
+       (mapv print-comment-bodies (:children comments-json))
+       nil)))
+
+
+(defn get-reddit-comments [link]
+  (go (let [response (<! (http/get (str link ".json")
+                                   {:with-credentials? false}))]
+        (prn (count (:body response)))
+        ;; mapv is NOT lazy, so we get prints right away!
+        (mapv print-comment-bodies (:body response)))))
+
 
 ;; -------------------------
 ;; State
@@ -15,10 +40,11 @@
 ;; Views
 
 (defn home-page []
+  (get-reddit-comments "https://www.reddit.com/r/Hydroponics/comments/p6jlip/growing_medium_falling_out_of_net_pots")
   (let [input-value (r/atom "foo")]
     (fn [] [:div [:h2 "Welcome to Reagent"]
             [:div [:p "The value: " @input-value] [:p "Change it: "] [atom-input input-value]]])))
-  
+
 
 ;; -------------------------
 ;; Initialize app
