4948565475a54a77e0d44a33c3201440980d3989
Kovas Palunas <kovas.palunas@gmail.com>
2021-11-07 15:19:58 -0800

    update release script to not create release version to avoid dragging bug

--- release.bash
+++ release.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-npx shadow-cljs release app
+# When I use
+# npx shadow-cljs release app
+# node dragging does not work for some reason.
+npx shadow-cljs compile app
+
 cp -r public/* release/
 rm release/index.html
 git add release/**
