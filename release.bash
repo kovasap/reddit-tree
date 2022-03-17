49c29a31c7113dfd77efdba41b3ce4e4d5f68fe8
Kovas Palunas <kovas.palunas@gmail.com>
2022-03-16 21:08:26 -0700

    fixed bad release issue when multiple apps are released to the same static site

--- release.bash
+++ release.bash
@@ -6,6 +6,11 @@
 npx shadow-cljs compile app
 
 cp -r public/* release/
+mkdir -p release/reddit-tree
+mv release/js release/reddit-tree/js
+mv release/css release/reddit-tree/css
+sed -i 's/\/css\/site.css/\/reddit-tree\/css\/site.css/g' release/reddit-tree.html
+sed -i 's/\/js\/app.js/\/reddit-tree\/js\/app.js/g' release/reddit-tree.html
 rm release/index.html
 git add release/**
 git commit -m "new release"
